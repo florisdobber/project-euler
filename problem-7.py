074026e4e783c5bef4df24f9bd4261982c5d7f67
unknown <florisdobber@gmail.com>
2020-11-29 16:48:38 -0500

    Problem 7 solution

---
+++ problem-7.py
@@ -0,0 +1,19 @@
+def main():
+    count = 0
+    i = 2
+    while True:
+        if is_prime(i):
+            count += 1
+        if count == 10001:
+            break
+        i += 1
+    print(i)
+    print(count)
+
+def is_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+main()
