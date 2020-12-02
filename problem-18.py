b3d4d12522e326a8903c0aa9c9624ed23b31ee2f
unknown <florisdobber@gmail.com>
2020-12-01 21:01:37 -0500

    Problem 18 & 67 solution

---
+++ problem-18.py
@@ -0,0 +1,19 @@
+def main():
+    pyramid = read_txt_file("resources/p18.txt")
+
+    for row in range(len(pyramid)-2, -1, -1):
+        for index in range(len(pyramid[row])):
+            best_choice = max(pyramid[row+1][index], pyramid[row+1][index+1])
+            pyramid[row][index] += best_choice
+
+    print(pyramid[0][0])
+
+def read_txt_file(path):
+    f = open(path, "r")
+    pyramid = [line.replace("\n", "").split(" ") for line in f]
+    pyramid = [[int(n) for n in line] for line in pyramid]
+    f.close()
+    return pyramid
+    
+
+main()
